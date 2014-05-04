09b8ae73fbb97b3ad8eab22b4aa08c00753918db
Bakró Nagy István <bakro.istvan@gmail.com>
2014-05-04 20:44:20 +0200

    alpha version

--- test1/src/hu/bakro/test1/hello.java
+++ test1/src/hu/bakro/test1/hello.java
@@ -1,7 +1,14 @@
 package hu.bakro.test1;
 
-public class hello {
+import javax.swing.JFrame;
+
+class hello {
 	public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
-    }
+		JFrame window = new JFrame("Tili-toli játék");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setContentPane(new ToliGui());
+		window.pack();  // finalize layout
+		window.show();  // make window visible
+		window.setResizable(false);
+	}
 }
